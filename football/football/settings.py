b0bc0325096fa11c6b48472edd421384a301d72d
peterwaf <peterwafulah@gmail.com>
2020-04-30 22:01:45 +0300

    Templates set up

--- football/football/settings.py
+++ football/football/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#points the location of the static folder
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'football/static')]
--- football/users/urls.py
+++ football/users/urls.py
@@ -1,5 +1,4 @@
-"""football URL Configuration
-
+"""my_bank URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -13,10 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
+app_name = 'users'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('users.urls')),
-]
+    path('register/',views.userRegistration,name='register'),]
