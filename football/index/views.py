923695e85c906cc214cf44d7b8163f0c025df85b
peterwaf <peterwafulah@gmail.com>
2020-05-06 00:51:02 +0300

    premium page templates modification

--- football/index/views.py
+++ football/index/views.py
@@ -5,27 +5,54 @@ from league.models import Leaguetype
 
 # Create your views here.
 
+#default views for free content
+
 def contentView(request):
-    #show only free content
-    free_posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
-    context = {'free_posts':free_posts}
+    #filter only free content, content_type = 0
+    posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
+    context = {'posts':posts}
     return render(request,"index/index.html",context)
 
 def detailedView(request,slug):
     singe_post = Post.objects.get(slug=slug)
-    free_posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
-    context = {'singe_post':singe_post,'free_posts':free_posts}
+    #filter only free content, content_type = 0
+    posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
+    context = {'singe_post':singe_post,'posts':posts}
     return render(request,"index/inner_page.html",context)
 
 def categorycontentView(request,league_id):
+    #filter only free content, content_type = 0 and league id
     category_posts = Post.objects.filter(status=1,content_type=0,pk=league_id).order_by('-created_on')
-    free_posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
+    posts = Post.objects.filter(status=1,content_type=0).order_by('-created_on')
     #initialise a single variable for each league category
     single_league_category = ''
     for league  in category_posts:
         single_league_category = league.league
-    context = {'category_posts':category_posts,'single_league_category':single_league_category,'free_posts':free_posts}
+    context = {'category_posts':category_posts,'single_league_category':single_league_category,'posts':posts}
     return render(request,"index/category_detail.html",context)
 
+#default views for premium content
 
+def premiumcontentView(request):
+    #filter only paid content, content_type = 1
+    posts = Post.objects.filter(status=1,content_type=1).order_by('-created_on')
+    context = {'posts':posts}
+    return render(request,"index/premium_index.html",context)
 
+def premiumdetailedView(request,slug):
+    singe_post = Post.objects.get(slug=slug)
+    #filter only paid content, content_type = 1
+    posts = Post.objects.filter(status=1,content_type=1).order_by('-created_on')
+    context = {'singe_post':singe_post,'posts':posts}
+    return render(request,"index/premium_inner_page.html",context)
+
+def premiumategorycontentView(request,league_id2):
+    #filter only paid content, content_type = 1
+    category_posts = Post.objects.filter(status=1,content_type=1,pk=league_id2).order_by('-created_on')
+    posts = Post.objects.filter(status=1,content_type=1,pk=league_id2).order_by('-created_on')
+    #initialise a single variable for each league category
+    single_league_category = ''
+    for league  in category_posts:
+        single_league_category = league.league
+    context = {'category_posts':category_posts,'single_league_category':single_league_category,'posts':posts}
+    return render(request,"index/premium_category_detail.html",context)
