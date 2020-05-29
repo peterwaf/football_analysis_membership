2c7be636df607edc494d58b0098bc1902741d295
peterwaf <peterwafulah@gmail.com>
2020-05-29 14:28:24 +0300

    creating a responsive dashboard template

--- football/dashboard/views.py
+++ football/dashboard/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def dashboard(request):
-    html = '<h1>This is a dashboard </h1>'
-    return HttpResponse('html')
+    context = {}
+    return render(request,"dashboard/dashboard.html",context)
