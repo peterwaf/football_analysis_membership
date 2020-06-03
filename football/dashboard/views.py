775c296e1cc4c84ad1e855ff4f2efd9ef7b9dfa5
peterwaf <peterwafulah@gmail.com>
2020-06-03 07:48:23 +0300

    Dashboard enhancement

--- football/dashboard/views.py
+++ football/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from content.models import Post
 from .forms import AddPosts
+from django.utils import timezone
 # Create your views here.
 def dashboard(request):
     #show all posts
@@ -13,6 +14,13 @@ def addPosts(request):
     form = AddPosts()
     if request.method == "POST":
         form = AddPosts(request.POST)
+    if form.is_valid():
+        Post = form.save(commit=False)
+        Post.created_on = timezone.now()
+        Post.author = request.user
+        Post.save()
+        return redirect('dashboard:dashboard')
+
     else:
         form = AddPosts()
     context = {'form':form}
